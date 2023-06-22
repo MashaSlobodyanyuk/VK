019361935e7702778437c83ed19944bb0458dc97
Masha S <mashaslobodyanyuk@gmail.com>
2023-06-22 13:03:50 +0300

    Innitaal commit

--- src/test/kotlin/WallServiceTest.kt
+++ src/test/kotlin/WallServiceTest.kt
@@ -7,11 +7,11 @@ class WallServiceTest {
     @Test
     fun add() {
 
-        var post1 = Post(id = 0, text = "Petya", date = 17, views = 18, geo = "Moscow", canPin = true, likes = Likes())
-        var post2 = post1.copy(id = 1)
+        val post1 = Post(id = 0, text = "Petya", date = 17, views = 18, geo = "Moscow", canPin = true, likes = Likes())
+        val post2 = post1.copy(id = 1)
 
         WallService.add(post1)
-        var getPost = WallService.add(post2)
+        val getPost = WallService.add(post2)
 
         assertEquals(post2.id, getPost.id)
 
@@ -19,21 +19,20 @@ class WallServiceTest {
 
     @Test
     fun updateFalse() {
-        var post3 = Post(id = 1500, text = "Petya", date = 17, views = 18, geo = "Moscow", canPin = true, likes = Likes())
-        var post4 = Post(id = 7000, text = "Petya", date = 17, views = 18, geo = "Moscow", canPin = true, likes = Likes())
+        val post3 = Post(id = 1500, text = "Petya", date = 17, views = 18, geo = "Moscow", canPin = true, likes = Likes())
 
 
 
-        var result = WallService.update(post3)
+        val result = WallService.update(post3)
         assertEquals(result, false)
     }
 
     @Test
     fun updateTrue() {
-        var post1 = Post(id = 800, text = "Petya", date = 17, views = 18, geo = "Moscow", canPin = true, likes = Likes())
-        var getPost = WallService.add(post1)
+        val post1 = Post(id = 800, text = "Petya", date = 17, views = 18, geo = "Moscow", canPin = true, likes = Likes())
+        val getPost = WallService.add(post1)
         println(getPost)
-        var result = WallService.update(getPost.copy(text="Vasia"))
+        val result = WallService.update(getPost.copy(text="Vasia"))
         assertEquals(result, true)
     }
 }
